7ad3cfffd61cbb7cad58b67653416cf1549d1294
juntae ha <hjt1031@naver.com>
2022-07-03 18:05:37 +0900

    탐색 위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -10,3 +10,9 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 }
+/**
+ * Controller : 스프링 MVC컨트롤러 인식
+ * Repository : 스프링 데이터 접근 계층으로 인식하고, 데이터 계층의 예외를 스프링 예외로 변환해준다
+ * Configuration : 앞서 보았듯이 스프링 설정 정보로 인식하고, 스프링 빈이 싱글톤을 유지하도록 추가 처리를 한다
+ * Service : 사실 service는 특별 처리 안함, 대신 개발자들이 핵심 비즈니스 로직이 여기 있겟구나 라고 인식하는데 도움 됨.
+ */
