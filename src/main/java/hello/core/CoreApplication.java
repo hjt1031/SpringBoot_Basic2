17ec89bfb2df6f3a62edc104a0fe5c1eb4cbe187
juntae ha <hjt1031@naver.com>
2023-01-02 18:10:27 +0900

    복습

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -23,5 +23,10 @@ public class CoreApplication {
 @Component
 1. 구현체 에다가 @Component 등록 하면 자동으로 스프링 컨테이너에 등록 됨
 2. @AutoWried 로 의존성 주입  = ac.getBean(MemberRepository.class) 라 생각하면 됨.
+
+
+상속
+부모 타입으로 조회하면, 자식 타입도 함께 조회된다.
+그래서 모든 자바 객체의 최고 부모인 Object 타입으로 조회하면, 모든 스프링 빈을 조회한다.
 * */
 
